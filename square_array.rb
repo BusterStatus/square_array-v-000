6748bee6b5c0f18be737691867eade200d280525
matthew.serre <matthew.serre@gmail.com>
2018-10-25 01:18:59 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,8 @@
 def square_array(array)
-  # your code here
-  new_array = []
-  array.each {|i| new_array << i ** 2}
-  new_array
+  # # your code here
+  # new_array = []
+  # array.each {|i| new_array << i ** 2}
+  # new_array
+  
+  
 end
